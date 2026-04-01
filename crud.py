bb48a500814b6624bbdbb152c16b408838241aa3
DevJoshCodes <devjoshcodes@gmail.com>
2026-04-01 17:44:50 +0800

    crud implemented

--- crud.py
+++ crud.py
@@ -17,10 +17,37 @@ def create_task(name, title, subject, due_date, description):
     result = tasks.insert_one(task)
     return str(result.inserted_id)
 
-create_task(
-    "Josh",
-    "Math Homework",
-    "Math",
-    datetime(2026, 4, 10),
-    "Complete exercises 1–10"
-)
+
+def get_all_tasks():
+    return list(tasks.find({}, {"_id": 0}))
+
+def get_tasks_by_name(name):
+    return list(tasks.find({"name": name}, {"_id": 0}))
+
+def get_task(title):
+    return tasks.find_one({"title": title}, {"_id": 0})
+
+def update_task(title, updated_data):
+    result = tasks.update_one(
+        {"title": title},
+        {"$set": updated_data}
+    )
+    
+    return result.modified_count
+
+def delete_task(title):
+    result = tasks.delete_one({"title": title})
+    return result.deleted_count
+
+
+# Create
+create_task("Bob", "Science Project", "Science", datetime(2026, 4, 15), "Build a volcano")
+
+# Read
+print(get_all_tasks())
+
+# Update
+update_task("Science Project", {"subject": "Physics"})
+
+# Delete
+delete_task("Science Project")
